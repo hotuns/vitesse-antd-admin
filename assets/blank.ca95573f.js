51ccf3cf7a43f60df4010d69f2596aa1ff2e5d5d
hds <hedongshu@foxmail.com>
2022-04-24 14:46:27 +0800

    update

--- assets/blank.ca95573f.js
+++
@@ -1,1 +0,0 @@
-import{k as o,g as r,r as n,o as t}from"./index.547a7a52.js";const c={};function a(s,_){const e=n("router-view");return t(),r(e)}var p=o(c,[["render",a]]);export{p as default};
---
+++ assets/readme.f990ae0d.js
@@ -0,0 +1,12 @@
+import{k as t,o as s,l as e,i as a,m as o}from"./index.d2ad968d.js";const p={class:"prose prose-sm m-auto text-left"},c=o(`<h1>\u8BF4\u660E</h1><pre class="language-html"><code class="language-html">
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>route</span> <span class="token attr-name">lang</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>yaml<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>
+meta:
+  layout: default
+  title: &#39;&#39;
+  icon: &#39;&#39;
+  auth: [user, admin]
+  hideInMenu: false
+  hideChildInMenu: false
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>route</span><span class="token punctuation">&gt;</span></span>
+
+</code></pre>`,2),l=[c],u={setup(r,{expose:n}){return n({frontmatter:{meta:[]}}),t({meta:[]}),(m,d)=>(s(),e("div",p,l))}};typeof a=="function"&&a(u);export{u as default};
